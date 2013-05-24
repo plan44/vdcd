d0eb95abab78f823d2252475f77b858ef479620a
Lukas Zeller <luz@plan44.ch>
2013-05-24 18:45:18 +0200

    ButtonBehaviour: implemented dS button state machine, untested so far

--- src/buttonbehaviour.cpp
+++ src/buttonbehaviour.cpp
@@ -14,13 +14,248 @@ using namespace p44;
 ButtonBehaviour::ButtonBehaviour(Device *aDeviceP) :
   inherited(aDeviceP)
 {
+  resetStateMachine();
 }
 
 
 void ButtonBehaviour::buttonAction(bool aPressed)
 {
   LOG(LOG_NOTICE,"ButtonBehaviour: Button was %s\n", aPressed ? "pressed" : "released");
+  buttonPressed = aPressed; // remember state
+  checkStateMachine(true, MainLoop::now());
 }
+
+
+void ButtonBehaviour::resetStateMachine()
+{
+  buttonPressed = false;
+  state = S0_idle;
+  clickCounter = 0;
+  holdRepeats = 0;
+  outputOn = false;
+  localButtonEnabled = false;
+  dimmingUp = false;
+  timerRef = 0;
+  timerPending = false;
+}
+
+
+
+void ButtonBehaviour::checkTimer(MLMicroSeconds aCycleStartTime)
+{
+  checkStateMachine(false, aCycleStartTime);
+  timerPending = false;
+}
+
+
+
+void ButtonBehaviour::checkStateMachine(bool aButtonChange, MLMicroSeconds aNow)
+{
+  if (timerPending) {
+    MainLoop::currentMainLoop()->cancelExecutionsFrom(this);
+    timerPending = false;
+  }
+  MLMicroSeconds timeSinceRef = aNow-timerRef;
+
+  switch (state) {
+
+    case S0_idle :
+      timerRef = 0; // no timer running
+      if (aButtonChange && buttonPressed) {
+        clickCounter = localButtonEnabled ? 0 : 1;
+        timerRef = aNow;
+        state = S1_initialpress;
+      }
+      break;
+
+    case S1_initialpress :
+      if (aButtonChange && !buttonPressed) {
+        timerRef = aNow;
+        state = S5_nextPauseWait;
+      }
+      else if (timeSinceRef>=t_click_length) {
+        state = S2_holdOrTip;
+      }
+      break;
+
+    case S2_holdOrTip:
+      if (aButtonChange && !buttonPressed && clickCounter==0) {
+        localToggle();
+        timerRef = aNow;
+        clickCounter = 1;
+        state = S4_nextTipWait;
+      }
+      else if (aButtonChange && !buttonPressed && clickCounter>0) {
+        sendClick((ClickType)(ct_tip_1x+clickCounter-1));
+        timerRef = aNow;
+        state = S4_nextTipWait;
+      }
+      else if (timeSinceRef>=t_long_function_delay) {
+        // long function
+        if (!localButtonEnabled || !outputOn) {
+          // hold
+          holdRepeats = 0;
+          timerRef = aNow;
+          sendClick(ct_hold_start);
+          state = S3_hold;
+        }
+        else if (localButtonEnabled && outputOn) {
+          // local dimming
+          dimmingUp = !dimmingUp; // change direction
+          timerRef = aNow+t_local_dim_timeout; // force first timeout right away
+          state = S11_localdim;
+        }
+      }
+      break;
+
+    case S3_hold:
+      if (aButtonChange && buttonPressed) {
+        // no packet send time, skip S15
+        sendClick(ct_hold_end);
+        state = S0_idle;
+      }
+      else if (timeSinceRef>=t_dim_repeat_time) {
+        if (holdRepeats<max_hold_repeats) {
+          timerRef = aNow;
+          sendClick(ct_hold_repeat);
+          holdRepeats++;
+        }
+        else {
+          sendClick(ct_hold_end);
+          state = S14_awaitrelease;
+        }
+      }
+      break;
+
+    case S4_nextTipWait:
+      if (aButtonChange && buttonPressed) {
+        timerRef = aNow;
+        if (clickCounter>=4)
+          clickCounter = 2;
+        else
+          clickCounter++;
+        state = S2_holdOrTip;
+      }
+      else if (timeSinceRef>=t_tip_timeout) {
+        state = S0_idle;
+      }
+      break;
+
+    case S5_nextPauseWait:
+      if (aButtonChange && buttonPressed) {
+        timerRef = aNow;
+        clickCounter = 2;
+        state = S6_2ClickWait;
+      }
+      else if (timeSinceRef>=t_click_pause) {
+        if (localButtonEnabled)
+          localToggle();
+        else
+          sendClick(ct_click_1x);
+        state = S4_nextTipWait;
+      }
+      break;
+
+    case S6_2ClickWait:
+      if (aButtonChange && !buttonPressed) {
+        timerRef = aNow;
+        state = S9_2pauseWait;
+      }
+      else if (timeSinceRef>t_click_length) {
+        state = S7_progModeWait;
+      }
+      break;
+
+    case S7_progModeWait:
+      if (aButtonChange && !buttonPressed) {
+        sendClick(ct_tip_2x);
+        timerRef = aNow;
+        state = S4_nextTipWait;
+      }
+      else if (timeSinceRef>t_long_function_delay) {
+        state = S8_awaitrelease;
+      }
+      break;
+
+    case S9_2pauseWait:
+      if (aButtonChange && buttonPressed) {
+        timerRef = aNow;
+        clickCounter = 3;
+        state = S12_3clickWait;
+      }
+      else if (timeSinceRef>=t_click_pause) {
+        sendClick(ct_click_2x);
+        state = S4_nextTipWait;
+      }
+      break;
+
+    case S12_3clickWait:
+      if (aButtonChange && !buttonPressed) {
+        timerRef = aNow;
+        sendClick(ct_click_3x);
+        state = S4_nextTipWait;
+      }
+      else if (timeSinceRef>=t_click_length) {
+        state = S13_3pauseWait;
+      }
+      break;
+
+    case S13_3pauseWait:
+      if (aButtonChange && !buttonPressed) {
+        timerRef = aNow;
+        sendClick(ct_tip_3x);
+      }
+      else if (timeSinceRef>=t_long_function_delay) {
+        sendClick(ct_short_short_long);
+        state = S8_awaitrelease;
+      }
+      break;
+
+    case S11_localdim:
+      if (aButtonChange && !buttonPressed) {
+        state = S0_idle;
+      }
+      else if (timeSinceRef>=t_dim_repeat_time) {
+        localDim();
+        timerRef = aNow;
+      }
+      break;
+
+    case S8_awaitrelease:
+    case S14_awaitrelease:
+      if (aButtonChange && !buttonPressed) {
+        state = S0_idle;
+      }
+      break;
+  }
+  if (timerRef) {
+    // need timing, schedule calling again
+    timerPending = true;
+    MainLoop::currentMainLoop()->executeOnceAt(boost::bind(&ButtonBehaviour::checkTimer, this, _2), aNow+10*MilliSecond, this);
+  }
+}
+
+
+
+void ButtonBehaviour::localToggle()
+{
+  LOG(LOG_NOTICE,"ButtonBehaviour: Local toggle\n");
+}
+
+
+void ButtonBehaviour::localDim()
+{
+  LOG(LOG_NOTICE,"ButtonBehaviour: Local dim\n");
+}
+
+
+
+void ButtonBehaviour::sendClick(ClickType aClickType)
+{
+  LOG(LOG_NOTICE,"ButtonBehaviour: Sending Click Type %d\n", aClickType);
+}
+
+
 
 
 
--- src/buttonbehaviour.hpp
+++ src/buttonbehaviour.hpp
@@ -18,6 +18,71 @@ namespace p44 {
   class ButtonBehaviour : public DSBehaviour
   {
     typedef DSBehaviour inherited;
+
+    // button state machine v2.01
+
+    // - states
+    typedef enum {
+      S0_idle,
+      S1_initialpress,
+      S2_holdOrTip,
+      S3_hold,
+      S4_nextTipWait,
+      S5_nextPauseWait,
+      S6_2ClickWait,
+      S7_progModeWait,
+      S8_awaitrelease,
+      S9_2pauseWait,
+      // S10 missing
+      S11_localdim,
+      S12_3clickWait,
+      S13_3pauseWait,
+      S14_awaitrelease, // duplicate of S8
+    } ButtonState;
+
+    // - click types
+    typedef enum {
+      ct_tip_1x,
+      ct_tip_2x,
+      ct_tip_3x,
+      ct_tip_4x,
+      ct_hold_start,
+      ct_hold_repeat,
+      ct_hold_end,
+      ct_click_1x,
+      ct_click_2x,
+      ct_click_3x,
+      ct_short_long,
+      ct_short_short_long
+    } ClickType;
+
+    // - vars
+    bool buttonPressed;
+    ButtonState state;
+    int clickCounter;
+    int holdRepeats;
+    bool outputOn;
+    bool localButtonEnabled;
+    bool dimmingUp;
+    MLMicroSeconds timerRef;
+    bool timerPending;
+    // - params
+    const int t_long_function_delay = 500*MilliSecond;
+    const int t_dim_repeat_time = 1000*MilliSecond;
+    const int t_click_length = 140*MilliSecond;
+    const int t_click_pause = 140*MilliSecond;
+    const int t_tip_timeout = 800*MilliSecond;
+    const int t_local_dim_timeout = 160*MilliSecond;
+    const int max_hold_repeats = 30*MilliSecond;
+
+    // - methods
+    void resetStateMachine();
+    void checkStateMachine(bool aButtonChange, MLMicroSeconds aNow);
+    void checkTimer(MLMicroSeconds aCycleStartTime);
+    void localToggle();
+    void localDim();
+    void sendClick(ClickType aClickType);
+
   public:
     // constructor
     ButtonBehaviour(Device *aDeviceP);
