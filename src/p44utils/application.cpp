9e653a8fd59d80eeba532f7b51666e3797b60767
Lukas Zeller <luz@plan44.ch>
2013-09-03 07:08:48 +0200

    p44utils: CmdLineApp subclass of Application added, containing full featured command line parser

--- src/p44utils/application.cpp
+++ src/p44utils/application.cpp
@@ -14,6 +14,7 @@
 
 using namespace p44;
 
+#pragma mark - Application base class
 
 static Application *sharedApplicationP = NULL;
 
@@ -45,7 +46,7 @@ Application::~Application()
 int Application::main(int argc, char **argv)
 {
 	// NOP application
-	exit(EXIT_SUCCESS);
+	return EXIT_SUCCESS;
 }
 
 
@@ -58,12 +59,20 @@ void Application::initialize()
 int Application::run()
 {
 	if (!mainLoopP)
-		return EXIT_SUCCESS; // NOP
+		terminateApp(EXIT_SUCCESS); // NOP App
 	// schedule the initialize() method as first mainloop method
 	mainLoopP->executeOnce(boost::bind(&Application::initialize, this));
 	// run the mainloop
 	return mainLoopP->run();
 }
+
+
+void Application::terminateApp(int aExitCode)
+{
+  // TODO: add more elaborated exit handling
+  exit(aExitCode);
+}
+
 
 
 void Application::daemonize()
@@ -105,3 +114,277 @@ void Application::daemonize()
   freopen( "/dev/null", "w", stdout);
   freopen( "/dev/null", "w", stderr);
 }
+
+
+#pragma mark - CmdLineApp command line application
+
+
+/// constructor
+CmdLineApp::CmdLineApp(MainLoop *aMainLoopP) :
+  inherited(aMainLoopP),
+  optionDescriptors(NULL)
+{
+}
+
+
+/// destructor
+CmdLineApp::~CmdLineApp()
+{
+}
+
+
+
+void CmdLineApp::setCommandDescriptors(const CmdLineOptionDescriptor *aOptionDescriptors, const char *aSynopsis)
+{
+  optionDescriptors = aOptionDescriptors;
+  synopsis = aSynopsis ? aSynopsis : "Usage: %1$s";
+}
+
+
+#define MAX_INDENT 20
+
+void CmdLineApp::showUsage()
+{
+  // print synopsis
+  fprintf(stderr, synopsis.c_str(), invocationName.c_str());
+  // print options
+  // - calculate indent
+  size_t indent = 0;
+  const CmdLineOptionDescriptor *optionDescP = optionDescriptors;
+  bool anyShortOpts = false;
+  while (optionDescP && (optionDescP->longOptionName!=NULL || optionDescP->shortOptionChar!='\x00')) {
+    if (optionDescP->shortOptionChar) {
+      anyShortOpts = true;
+    }
+    size_t n = 0;
+    if (optionDescP->longOptionName) {
+      n += strlen(optionDescP->longOptionName)+2; // "--XXXXX"
+    }
+    const char *desc = optionDescP->optionDescription;
+    if (optionDescP->withArgument && desc) {
+      const char *p = strchr(desc, ':');
+      if (p) {
+        n += 1 + (p-desc); // add room for argument description
+      }
+    }
+    if (n>MAX_INDENT) n = MAX_INDENT;
+    if (n>indent) indent = n; // new max
+    optionDescP++;
+  }
+  if (anyShortOpts) indent += 4; // "-X, " prefix
+  indent += 2 + 2; // two at beginning, two at end
+  // - print options
+  fprintf(stderr, "Options:\n");
+  optionDescP = optionDescriptors;
+  while (optionDescP && (optionDescP->longOptionName!=NULL || optionDescP->shortOptionChar!='\x00')) {
+    size_t remaining = indent;
+    fprintf(stderr, "  "); // start indent
+    remaining -= 2;
+    if (anyShortOpts) {
+      // short names exist, print them for those options that have them
+      if (optionDescP->shortOptionChar)
+        fprintf(stderr, "-%c", optionDescP->shortOptionChar);
+      else
+        fprintf(stderr, "  ");
+      remaining -= 2;
+      if (optionDescP->longOptionName) {
+        // long option follows, fill up
+        if (optionDescP->shortOptionChar)
+          fprintf(stderr, ", ");
+        else
+          fprintf(stderr, "  ");
+        remaining -= 2;
+      }
+    }
+    // long name
+    if (optionDescP->longOptionName) {
+      fprintf(stderr, "--%s", optionDescP->longOptionName);
+      remaining -= strlen(optionDescP->longOptionName);
+    }
+    // argument
+    const char *desc = optionDescP->optionDescription;
+    if (optionDescP->withArgument) {
+      if (desc) {
+        const char *p = strchr(desc, ':');
+        if (p) {
+          size_t n = (p-desc);
+          string argDesc(desc,n);
+          fprintf(stderr, " %s", argDesc.c_str());
+          remaining -= argDesc.length()+1;
+          desc += n+1; // desc starts after colon
+        }
+      }
+    }
+    // complete first line indent
+    while (remaining>0) fprintf(stderr, " ");
+    // print option description, properly indented
+    if (desc) {
+      while (*desc) {
+        if (*desc=='\n') {
+          // next line
+          fprintf(stderr, "\n");
+          // indent
+          remaining = indent;
+          while (remaining>0) fprintf(stderr, " ");
+        }
+        else {
+          fprintf(stderr, "%c", *desc);
+        }
+        desc++;
+      }
+    }
+    // end of option, next line
+    fprintf(stderr, "\n");
+  }
+  fprintf(stderr, "\n");
+}
+
+
+
+void CmdLineApp::parseCommandLine(int aArgc, char **aArgv)
+{
+  if (aArgc>0) {
+    invocationName = aArgv[0];
+    int rawArgIndex=1;
+    while(rawArgIndex<aArgc) {
+      const char *argP = aArgv[rawArgIndex];
+      if (*argP=='-') {
+        // option argument
+        argP++;
+        bool longOpt = false;
+        string optName;
+        string optArg;
+        bool optArgFound = false;
+        if (*argP=='-') {
+          // long option
+          longOpt = true;
+          optName = argP+1;
+          if (optName=="help") {
+            showUsage();
+            terminateApp(EXIT_SUCCESS);
+          }
+        }
+        else {
+          // short option
+          optName = argP;
+          if (optName=="h") {
+            showUsage();
+            terminateApp(EXIT_SUCCESS);
+          }
+          if (optName.length()>1 && optName[1]!='=') {
+            // option argument follows directly after single char option
+            optArgFound = true; // is non-empty by definition
+            optArg = optName.substr(1,string::npos);
+            optName.erase(1,string::npos);
+          }
+        }
+        // search for option argument directly following option separated by equal sign
+        string::size_type n = optName.find_first_of('=');
+        if (n!=string::npos) {
+          optArgFound = true; // explicit specification, counts as option argument even if empty string
+          optArg = optName.substr(n+1,string::npos);
+          optName.erase(n,string::npos);
+        }
+        // search for option descriptor
+        const CmdLineOptionDescriptor *optionDescP = optionDescriptors;
+        bool optionFound = false;
+        while (optionDescP && (optionDescP->longOptionName!=NULL || optionDescP->shortOptionChar!='\x00')) {
+          // not yet end of descriptor list
+          if (
+            (longOpt && optName==optionDescP->longOptionName) ||
+            (!longOpt && optName[0]==optionDescP->shortOptionChar)
+          ) {
+            // option match found
+            if (!optionDescP->withArgument) {
+              // option without argument
+              if (optArgFound) {
+                fprintf(stderr, "Option '%s' does not expect an argument\n", optName.c_str());
+                showUsage();
+                terminateApp(EXIT_FAILURE);
+              }
+            }
+            else {
+              // option with argument
+              if (!optArgFound) {
+                // check for next arg as option arg
+                if (rawArgIndex<aArgc-1) {
+                  // there is a next argument, use it as option argument
+                  optArgFound = true;
+                  optArg = aArgv[rawArgIndex++];
+                }
+              }
+              if (!optArgFound) {
+                fprintf(stderr, "Option '%s' requires an argument\n", optName.c_str());
+                showUsage();
+                terminateApp(EXIT_FAILURE);
+              }
+            }
+            // now have option processed by subclass
+            if (!processOption(*optionDescP, optArg.c_str())) {
+              // not processed, store instead
+              if (optionDescP->longOptionName)
+                optName = optionDescP->longOptionName;
+              else
+                optName[0] = optionDescP->shortOptionChar;
+              // save in map
+              options[optName] = optArg;
+            }
+            optionFound = true;
+            break;
+          }
+          // next in list
+          optionDescP++;
+        }
+        if (!optionFound) {
+          fprintf(stderr, "Unknown Option '%s'\n", optName.c_str());
+          showUsage();
+          terminateApp(EXIT_FAILURE);
+        }
+      }
+      else {
+        // non-option argument
+        // - have argument processed by subclass
+        if (!processArgument(argP)) {
+          // not processed, store instead
+          arguments.push_back(argP);
+        }
+      }
+      // next argument
+      rawArgIndex++;
+    }
+  }
+}
+
+
+size_t CmdLineApp::numArguments()
+{
+  return arguments.size();
+}
+
+
+void CmdLineApp::resetCommandLine()
+{
+  invocationName.clear();
+  synopsis.clear();
+  options.clear();
+  arguments.clear();
+}
+
+
+const char *CmdLineApp::getOption(const char *aOptionName)
+{
+  const char *opt = NULL;
+  OptionsMap::iterator pos = options.find(aOptionName);
+  if (pos!=options.end()) {
+    opt = pos->second.c_str();
+  }
+  return opt;
+}
+
+
+const char *CmdLineApp::getArgument(size_t aArgumentIndex)
+{
+  if (aArgumentIndex>arguments.size()) return NULL;
+  return arguments[aArgumentIndex].c_str();
+}
+
--- src/p44utils/application.hpp
+++ src/p44utils/application.hpp
@@ -23,22 +23,128 @@ namespace p44 {
   public:
     /// constructor
     Application(MainLoop *aMainLoopP);
+
     /// default constructor
     Application();
+
     /// destructor
     virtual ~Application();
+
     /// main routine
+    /// @param argc argument count as passed to C-level main() entry point
+    /// @param argv argument pointer array as passed to C-level main() entry point
     virtual int main(int argc, char **argv);
+
     /// get shared instance (singleton)
     Application *sharedApplication();
+
+    /// terminate app
+    /// @param aExitCode the exit code to return to the parent
+    void terminateApp(int aExitCode);
+
   protected:
+
     /// daemonize
     void daemonize();
+
     /// start running the app's main loop
     int run();
+
     /// scheduled to run when mainloop has started
     virtual void initialize();
   };
+
+
+  /// Command line option descriptor
+  /// @note a descriptor with both longOptionName==NULL and shortOptionChar=0 terminates a list of option descriptors
+  typedef struct {
+    bool withArgument; ///< true if option has an argument (separated by = or next argument)
+    const char *longOptionName; ///< the long option name (string) or NULL if none
+    char shortOptionChar; ///< the short option name (single character) or 0/NUL if none
+    const char *optionDescription; ///< the description of the option, can have multiple lines separated by \n
+    int optionIdentifier; ///< an optional identifier
+  } CmdLineOptionDescriptor;
+
+  typedef vector<string> ArgumentsVector;
+  typedef map<string,string> OptionsMap;
+
+  class CmdLineApp : public Application
+  {
+    typedef Application inherited;
+
+    const CmdLineOptionDescriptor *optionDescriptors;
+
+    string invocationName;
+    string synopsis;
+    OptionsMap options;
+    ArgumentsVector arguments;
+
+  public:
+
+    /// constructor
+    CmdLineApp(MainLoop *aMainLoopP);
+
+    /// destructor
+    virtual ~CmdLineApp();
+
+  protected:
+
+    /// set command description constants (option definitions and synopsis)
+    /// @param aCmdLineOptionDescriptors pointer to array of descriptors for the options
+    /// @param aSynopsis short usage description, used in showUsage(). %1$s will be replaced by invocationName
+    void setCommandDescriptors(const CmdLineOptionDescriptor *aOptionDescriptors, const char *aSynopsis = NULL);
+
+    /// show usage, consisting of invocationName + synopsis + option descriptions
+    void showUsage();
+
+    /// parse command line.
+    /// @param aArgc argument count as passed to C-level main() entry point
+    /// @param aArgv argument pointer array as passed to C-level main() entry point
+    /// @note setOptionDescriptors() must be called before using this method
+    /// @note this method might call terminateApp() in case of command line syntax errors
+    /// @note "-h" and "--help" options will always show usage and terminate the app with EXIT_SUCCESS exit code
+    void parseCommandLine(int aArgc, char **aArgv);
+
+    /// reset internal argument lists (to save memory when arguments are all processed)
+    void resetCommandLine();
+
+    /// process a command line option. Override this to implement processing command line options
+    /// @param aOptionDescriptor the descriptor of the option
+    /// @param aOptionValue the value of the option, empty string if option has no value
+    /// @return true if option has been processed; false if option should be stored for later reference via getOption()
+    /// @note will be called from parseCommandLine()
+    virtual bool processOption(const CmdLineOptionDescriptor &aOptionDescriptor, const char *aOptionValue);
+
+    /// process a non-option command line argument
+    /// @param aArgument non-option argument
+    /// @return true if argument has been processed; false if argument should be stored for later reference via getArgument()
+    /// @note will be called from parseCommandLine()
+    virtual bool processArgument(const char *aArgument) { return false; /* not processed, store */ };
+
+    /// get app invocation name
+    /// @return application invocation name (argv[0])
+    /// @note parseCommandLine() must be called before using this method
+    const char *getInvocationName();
+
+    /// get option
+    /// @param aOptionName the name of the option (longOptionName if exists, shortOptionChar if no longOptionName exists)
+    /// @return NULL if option was not specified on the command line, empty string for options without argument, option's argument otherwise
+    /// @note parseCommandLine() must be called before using this method
+    const char *getOption(const char *aOptionName);
+
+    /// get non-option argument
+    /// @param aArgumentIndex the index of the argument (0=first non-option argument, 1=second non-option argument, etc.)
+    /// @return NULL if aArgumentIndex>=numArguments(), argument otherwise
+    /// @note parseCommandLine() must be called before using this method
+    const char *getArgument(size_t aArgumentIndex);
+
+    /// get number of (non-processed) arguments
+    /// @return number of arguments not already processed by processArgument() returning true
+    /// @note parseCommandLine() must be called before using this method
+    size_t numArguments();
+
+  };
+
 
 } // namespace p44
 
