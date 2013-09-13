plan44/vdcd

commit f15028fa1faf254db3b1011dbafedd47a61f976a

Name propagation implemented, now hue lights get name updates from dSS configurator, but prevents name truncation (name will not change when vdSM sends 20-char long truncated version of name that is already set)

src/huecomm.cpp
@@ -301,6 +301,7 @@ public:
               if (refind) {
                 // that's my known hue bridge, save the URL and report success
                 hueComm.baseURL = url; // save it
+                hueComm.apiReady = true; // can use API now
                 callback(hueComm, ErrorPtr()); // success
                 keepAlive.reset(); // will delete object if nobody else keeps it
                 return; // done
@@ -371,6 +372,7 @@ public:
         hueComm.userName = u->stringValue();
         hueComm.uuid = currentAuthCandidate->first;
         hueComm.baseURL = currentAuthCandidate->second;
+        hueComm.apiReady = true; // can use API now
         DBGLOG(LOG_DEBUG, "Bridge %s @ %s: successfully registered as user %s\n", hueComm.uuid.c_str(), hueComm.baseURL.c_str(), hueComm.userName.c_str());
         // successfully registered with hue bridge, let caller know
         callback(hueComm, ErrorPtr());
@@ -397,7 +399,8 @@ public:
 HueComm::HueComm() :
   inherited(SyncIOMainLoop::currentMainLoop()),
   bridgeAPIComm(SyncIOMainLoop::currentMainLoop()),
-  findInProgress(false)
+  findInProgress(false),
+  apiReady(false)
 {
 }
 
@@ -415,6 +418,9 @@ void HueComm::apiQuery(const char* aUrlSuffix, HueApiResultCB aResultHandler)
 
 void HueComm::apiAction(HttpMethods aMethod, const char* aUrlSuffix, JsonObjectPtr aData, HueApiResultCB aResultHandler, bool aNoAutoURL)
 {
+  if (!apiReady) {
+    if (aResultHandler) aResultHandler(*this,JsonObjectPtr(),ErrorPtr(new HueCommError(HueCommErrorApiNotReady)));
+  }
   string url;
   if (aNoAutoURL) {
     url = aUrlSuffix;
@@ -450,6 +456,7 @@ void HueComm::stopFind()
 
 void HueComm::refindBridge(HueBridgeFindCB aFindHandler)
 {
+  apiReady = false; // not yet found, API disabled
   BridgeFinderPtr bridgeFinder = BridgeFinderPtr(new BridgeFinder(*this, aFindHandler));
   bridgeFinder->refindBridge(aFindHandler);
 };

src/consoledevice.cpp
@@ -37,7 +37,7 @@ ConsoleDevice::ConsoleDevice(StaticDeviceContainer *aClassContainerP, const stri
     }
   }
   // assign name
-  setName(name);
+  initializeName(name);
   // create I/O
   if (hasOutput) {
     // Simulate light device

src/device.hpp
@@ -74,34 +74,49 @@ namespace p44 {
     /// @note devices can
     virtual int idBlockSize() { return 1; }; // normal devices only reserve one single ID (their own)
 
+    /// @return human readable model name/short description
+    virtual string modelName() { return "vdSD - virtual device"; }
+
+    /// @return the entity type (one of dSD|vdSD|vDC|dSM|vdSM|dSS|*)
+    virtual const char *entityType() { return "vdSD"; }
+
     /// @}
 
 
 
-    /// @name interface towards actual device hardware (or simulation)
+    /// @name interfaces for actual device hardware (or simulation)
     /// @{
-
-    /// set user assignable name
-    /// @param new name of the addressable entity
-    virtual void setName(const string &aName);
 
     /// set basic device color
     /// @param aColorGroup color group number
     void setPrimaryGroup(DsGroup aColorGroup);
+
+    /// set group membership
+    /// @param aColorGroup color group number to set or remove
+    /// @param aIsMember true to make device member of this group
+    void setGroupMembership(DsGroup aColorGroup, bool aIsMember);
+
+    /// report that device has vanished (disconnected without being told so via vDC API)
+    /// This will call disconnect() on the device, and remove it from all vDC container lists
+    /// @param aForgetParams if set, not only the connection to the device is removed, but also all parameters related to it
+    ///   such that in case the same device is re-connected later, it will not use previous configuration settings, but defaults.
+    /// @note this method should be called when bus scanning or other HW-side events detect disconnection
+    ///   of a device, such that it can be reported to the dS system.
+    /// @note calling hasVanished() might delete the object, so don't rely on 'this' after calling it unless you
+    ///   still hold a DevicePtr to it
+    void hasVanished(bool aForgetParams);
+
+    /// @}
+
 
     /// check group membership
     /// @param aColorGroup color group number to check
     /// @return true if device is member of this group
     bool isMember(DsGroup aColorGroup);
 
-    /// set group membership
-    /// @param aColorGroup color group number to check
-    /// @param aIsMember true to make device member of this group
-    void setGroupMembership(DsGroup aColorGroup, bool aIsMember);
-
-    /// @}
-
-
+    /// set user assignable name
+    /// @param new name of the addressable entity
+    virtual void setName(const string &aName);
 
     /// get reference to device container
     DeviceContainer &getDeviceContainer() { return classContainerP->getDeviceContainer(); };
@@ -119,33 +134,25 @@ namespace p44 {
     virtual ErrorPtr forget();
 
 
-    typedef boost::function<void (DevicePtr aDevice, bool aDisconnected)> DisconnectCB;
 
-    /// disconnect device. If presence is represented by data stored in the vDC rather than
-    /// detection of real physical presence on a bus, this call must clear the data that marks
-    /// the device as connected to this vDC (such as a learned-in enOcean button).
-    /// For devices where the vDC can be *absolutely certain* that they are still connected
-    /// to the vDC AND cannot possibly be connected to another vDC as well, this call should
-    /// return false.
-    /// @param aForgetParams if set, not only the connection to the device is removed, but also all parameters related to it
-    ///   such that in case the same device is re-connected later, it will not use previous configuration settings, but defaults.
-    /// @param aDisconnectResultHandler will be called to report true if device could be disconnected,
-    ///   false in case it is certain that the device is still connected to this and only this vDC
-    /// @note at the time aDisconnectResultHandler is called, the only owner left for the device object might be the
-    ///   aDevice argument to the DisconnectCB handler.
-    virtual void disconnect(bool aForgetParams, DisconnectCB aDisconnectResultHandler);
+    /// @name API implementation
 
+    /// @{
 
-    /// report that device has vanished (disconnected without being told so via vDC API)
-    /// This will call disconnect() on the device, and remove it from all vDC container lists
-    /// @param aForgetParams if set, not only the connection to the device is removed, but also all parameters related to it
-    ///   such that in case the same device is re-connected later, it will not use previous configuration settings, but defaults.
-    /// @note this method should be called when bus scanning or other HW-side events detect disconnection
-    ///   of a device, such that it can be reported to the dS system.
-    /// @note calling hasVanished() might delete the object, so don't rely on 'this' after calling it unless you
-    ///   still hold a DevicePtr to it
-    void hasVanished(bool aForgetParams);
+    /// called to let device handle device-level methods
+    /// @param aMethod the method
+    /// @param aJsonRpcId the id parameter to be used in sendResult()
+    /// @param aParams the parameters object
+    /// @note the parameters object always contains the dSID parameter which has been
+    ///   used already to route the method call to this device.
+    virtual ErrorPtr handleMethod(const string &aMethod, const string &aJsonRpcId, JsonObjectPtr aParams);
 
+    /// called to let device handle device-level notification
+    /// @param aMethod the notification
+    /// @param aParams the parameters object
+    /// @note the parameters object always contains the dSID parameter which has been
+    ///   used already to route the notification to this device.
+    virtual void handleNotification(const string &aMethod, JsonObjectPtr aParams);
 
     /// call scene on this device
     /// @param aSceneNo the scene to call.
@@ -165,26 +172,6 @@ namespace p44 {
     ///   this can be anything.
     virtual void identifyToUser();
 
-
-    /// @name DsAddressable API implementation
-
-    /// @{
-
-    /// called to let device handle device-level methods
-    /// @param aMethod the method
-    /// @param aJsonRpcId the id parameter to be used in sendResult()
-    /// @param aParams the parameters object
-    /// @note the parameters object always contains the dSID parameter which has been
-    ///   used already to route the method call to this device.
-    virtual ErrorPtr handleMethod(const string &aMethod, const string &aJsonRpcId, JsonObjectPtr aParams);
-
-    /// called to let device handle device-level notification
-    /// @param aMethod the notification
-    /// @param aParams the parameters object
-    /// @note the parameters object always contains the dSID parameter which has been
-    ///   used already to route the notification to this device.
-    virtual void handleNotification(const string &aMethod, JsonObjectPtr aParams);
-
     /// @}
 
 
@@ -203,18 +190,23 @@ namespace p44 {
     ///   output behaviours to collect data for an outgoing message.
     virtual void updateOutputValue(OutputBehaviour &aOutputBehaviour) { /* NOP */ };
 
-    /// @}
 
+    typedef boost::function<void (DevicePtr aDevice, bool aDisconnected)> DisconnectCB;
 
-    /// @name identification of the addressable entity
-    /// @{
+    /// disconnect device. If presence is represented by data stored in the vDC rather than
+    /// detection of real physical presence on a bus, this call must clear the data that marks
+    /// the device as connected to this vDC (such as a learned-in enOcean button).
+    /// For devices where the vDC can be *absolutely certain* that they are still connected
+    /// to the vDC AND cannot possibly be connected to another vDC as well, this call should
+    /// return false.
+    /// @param aForgetParams if set, not only the connection to the device is removed, but also all parameters related to it
+    ///   such that in case the same device is re-connected later, it will not use previous configuration settings, but defaults.
+    /// @param aDisconnectResultHandler will be called to report true if device could be disconnected,
+    ///   false in case it is certain that the device is still connected to this and only this vDC
+    /// @note at the time aDisconnectResultHandler is called, the only owner left for the device object might be the
+    ///   aDevice argument to the DisconnectCB handler.
+    virtual void disconnect(bool aForgetParams, DisconnectCB aDisconnectResultHandler);
 
-    /// @return human readable model name/short description
-    virtual string modelName() { return "vdSD - virtual device"; }
-
-    /// @return the entity type (one of dSD|vdSD|vDC|dSM|vdSM|dSS|*)
-    virtual const char *entityType() { return "vdSD"; }
-    
     /// @}
 
 

src/dsaddressable.cpp
@@ -25,6 +25,24 @@ DsAddressable::~DsAddressable()
 }
 
 
+void DsAddressable::setName(const string &aName)
+{
+  // TODO: for now dsm API truncates names to 20 bytes. Therefore,
+  //   we prevent replacing a long name with a truncated version
+  if (name!=aName && (name.length()<20 || name.substr(0,20)!=aName)) {
+    name = aName;
+  }
+}
+
+
+void DsAddressable::initializeName(const string &aName)
+{
+  // just assign
+  name = aName;
+}
+
+
+
 #pragma mark - vDC API
 
 

src/dsaddressable.hpp
@@ -58,7 +58,14 @@ namespace p44 {
 
     /// set user assignable name
     /// @param new name of the addressable entity
-    virtual void setName(const string &aName) { name = aName; };
+    /// @note might prevent truncating names (just shortening an otherwise unchanged name)
+    /// @note subclasses might propagate the name into actual device hardware (e.g. hue)
+    virtual void setName(const string &aName);
+
+    /// initialize user assignable name with a default name or a name obtained from hardware
+    /// @note use setName to change a name from the API or UI, as initializeName() does not
+    ///   propagate to hardware
+    void initializeName(const string &aName);
 
     /// @name vDC API
     /// @{

src/huecomm.hpp
@@ -25,6 +25,7 @@ namespace p44 {
     HueCommErrorOK,
     HueCommErrorReservedForBridge = 1, ///< 1..999 are native bridge error codes
     HueCommErrorUuidNotFound = 1000, ///< bridge specified by uuid was not found
+    HueCommErrorApiNotReady, ///< API not ready (bridge not yet found, no bridge paired)
     HueCommErrorDescription, ///< SSDP by uuid did find a device, but XML description was inaccessible or invalid
     HueCommErrorInvalidUser, ///< bridge did not allow accessing the API with the username
     HueCommErrorNoRegistration, ///< could not register with a bridge
@@ -105,6 +106,7 @@ namespace p44 {
     friend class BridgeFinder;
 
     bool findInProgress;
+    bool apiReady;
 
   public:
 

src/huedevice.cpp
@@ -351,11 +351,27 @@ HueComm &HueDevice::hueComm()
 
 
 
+void HueDevice::setName(const string &aName)
+{
+  string oldname = getName();
+  inherited::setName(aName);
+  if (getName()!=oldname) {
+    // really changed, propagate to hue
+    JsonObjectPtr params = JsonObject::newObj();
+    params->add("name", JsonObject::newString(getName()));
+    string url = string_format("/lights/%s", lightID.c_str());
+    hueComm().apiAction(httpMethodPUT, url.c_str(), params, NULL);
+  }
+}
+
+
+
+
 void HueDevice::initializeDevice(CompletedCB aCompletedCB, bool aFactoryReset)
 {
   // query light attributes and state
   string url = string_format("/lights/%s", lightID.c_str());
-  hueDeviceContainer().hueComm.apiQuery(url.c_str(), boost::bind(&HueDevice::deviceStateReceived, this, aCompletedCB, aFactoryReset, _2, _3));
+  hueComm().apiQuery(url.c_str(), boost::bind(&HueDevice::deviceStateReceived, this, aCompletedCB, aFactoryReset, _2, _3));
 }
 
 
@@ -400,7 +416,7 @@ void HueDevice::checkPresence(PresenceCB aPresenceResultHandler)
 {
   // query the device
   string url = string_format("/lights/%s", lightID.c_str());
-  hueDeviceContainer().hueComm.apiQuery(url.c_str(), boost::bind(&HueDevice::presenceStateReceived, this, aPresenceResultHandler, _2, _3));
+  hueComm().apiQuery(url.c_str(), boost::bind(&HueDevice::presenceStateReceived, this, aPresenceResultHandler, _2, _3));
 }
 
 
@@ -439,7 +455,7 @@ void HueDevice::alertHandler(int aLeftCycles)
   string url = string_format("/lights/%s/state", lightID.c_str());
   JsonObjectPtr newState = JsonObject::newObj();
   newState->add("alert", JsonObject::newString("select"));
-  hueDeviceContainer().hueComm.apiAction(httpMethodPUT, url.c_str(), newState, NULL);
+  hueComm().apiAction(httpMethodPUT, url.c_str(), newState, NULL);
   // schedule next if any left
   if (--aLeftCycles>0) {
     MainLoop::currentMainLoop().executeOnce(boost::bind(&HueDevice::alertHandler, this, aLeftCycles), 1*Second);
@@ -520,7 +536,7 @@ void HueDevice::updateOutputValue(OutputBehaviour &aOutputBehaviour)
     // for on and off, set transition time (1/10 second resolution)
     newState->add("transitiontime", JsonObject::newInt64(aOutputBehaviour.transitionTimeForHardware()/(100*MilliSecond)));
     LOG(LOG_DEBUG, "HueDevice: setting new state, brightness = %d, JSON = %s\n", b, newState->c_strValue());
-    hueDeviceContainer().hueComm.apiAction(httpMethodPUT, url.c_str(), newState, NULL);
+    hueComm().apiAction(httpMethodPUT, url.c_str(), newState, NULL);
   }
   else
     return inherited::updateOutputValue(aOutputBehaviour); // let superclass handle this

src/huedevice.hpp
@@ -140,6 +140,11 @@ namespace p44 {
     /// @return textual description of object
     virtual string description();
 
+    /// set user assignable name
+    /// @param new name of the hue device
+    /// @note will propagate the name to the hue bridge to name the light itself
+    virtual void setName(const string &aName);
+
 
     /// @name interaction with subclasses, actually representing physical I/O
     /// @{

commit 8c08fd22baa0ddde1fde56e288beef56ef7a60c4

hue: fixed discovery which was broken by f15028fa1f (Name propagation implemented...)

src/huecomm.cpp
@@ -418,7 +418,7 @@ void HueComm::apiQuery(const char* aUrlSuffix, HueApiResultCB aResultHandler)
 
 void HueComm::apiAction(HttpMethods aMethod, const char* aUrlSuffix, JsonObjectPtr aData, HueApiResultCB aResultHandler, bool aNoAutoURL)
 {
-  if (!apiReady) {
+  if (!apiReady && !aNoAutoURL) {
     if (aResultHandler) aResultHandler(*this,JsonObjectPtr(),ErrorPtr(new HueCommError(HueCommErrorApiNotReady)));
   }
   string url;