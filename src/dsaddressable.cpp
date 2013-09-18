plan44/vdcd

commit ff430e6bd8e9bac4cc53eabf2d66cbc48e905182

Removed "dsProfileVersion" property from vDC API as it appears to be a dS 1.0 specific thing and makes no sense at this time to define in vdSDs. The vdSM will report a hardcoded 3.5.0 upstreams to satisfy some checks in the Configurator UI.

src/dsaddressable.cpp
@@ -245,7 +245,6 @@ const PropertyDescriptor *DsAddressable::getPropertyDescriptor(int aPropIndex, i
   static const PropertyDescriptor properties[numDsAddressableProperties] = {
     { "dSID", ptype_charptr, false, dSID_key, &dsAddressable_key },
     { "model", ptype_charptr, false, model_key, &dsAddressable_key },
-    { "dsProfileVersion", ptype_int32, false, dsProfileVersion_key, &dsAddressable_key },
     { "hardwareVersion", ptype_charptr, false, hardwareVersion_key, &dsAddressable_key },
     { "hardwareGuid", ptype_charptr, false, hardwareGUID_key, &dsAddressable_key },
     { "numDevicesInHW", ptype_int32, false, numDevicesInHW_key, &dsAddressable_key },
@@ -273,7 +272,6 @@ bool DsAddressable::accessField(bool aForWrite, JsonObjectPtr &aPropValue, const
       switch (aPropertyDescriptor.accessKey) {
         case dSID_key: aPropValue = JsonObject::newString(dsid.getString()); return true;
         case model_key: aPropValue = JsonObject::newString(modelName()); return true;
-        case dsProfileVersion_key: aPropValue = JsonObject::newInt32(dsProfileVersion()); return true;
         case hardwareVersion_key: aPropValue = JsonObject::newString(hardwareVersion(), true); return true;
         case hardwareGUID_key: aPropValue = JsonObject::newString(hardwareGUID(), true); return true;
         case oemGUID_key: aPropValue = JsonObject::newString(oemGUID(), true); return true;

src/device.cpp
@@ -301,7 +301,6 @@ ErrorPtr Device::forget()
 
 enum {
   // device level simple parameters
-  dsProfileVersion_key,
   primaryGroup_key,
   isMember_key,
   progMode_key,
@@ -336,7 +335,6 @@ const PropertyDescriptor *Device::getPropertyDescriptor(int aPropIndex, int aDom
 {
   static const PropertyDescriptor properties[numDeviceProperties] = {
     // common device properties
-    { "dsProfileVersion", ptype_int32, false, dsProfileVersion_key, &device_key },
     { "primaryGroup", ptype_int8, false, primaryGroup_key, &device_key },
     { "isMember", ptype_bool, true, isMember_key, &device_key },
     { "progMode", ptype_bool, false, progMode_key, &device_key },
@@ -498,9 +496,6 @@ bool Device::accessField(bool aForWrite, JsonObjectPtr &aPropValue, const Proper
     else if (!aForWrite) {
       // read properties
       switch (aPropertyDescriptor.accessKey) {
-        case dsProfileVersion_key:
-          aPropValue = JsonObject::newInt32(dsProfileVersion());
-          return true;
         case primaryGroup_key:
           aPropValue = JsonObject::newInt32(primaryGroup);
           return true;

src/devicecontainer.hpp
@@ -205,9 +205,6 @@ namespace p44 {
     /// @return the entity type (one of dSD|vdSD|vDC|dSM|vdSM|dSS|*)
     virtual const char *entityType() { return "vDC"; }
 
-    /// @return 0xMMmmrrrr digitalstrom Profile version (MM=major, mm=minor, rrrr=revision)
-    virtual uint32_t dsProfileVersion() { return 0; }
-
     /// @return hardware version string or NULL if none
     virtual string hardwareVersion() { return ""; }
 

src/dsaddressable.hpp
@@ -146,9 +146,6 @@ namespace p44 {
     /// @return the entity type (one of dSD|vdSD|vDC|dSM|vdSM|dSS|*)
     virtual const char *entityType() { return "*"; }
 
-    /// @return 0xMMmmrrrr digitalstrom Profile version (MM=major, mm=minor, rrrr=revision)
-    virtual uint32_t dsProfileVersion() { return 0; }
-
     /// @return hardware version string or NULL if none
     virtual string hardwareVersion() { return ""; }
 

commit 94d37800794fc81498432f4ab5c11a6710cf1134

Fixed bug in ff430e6bd8 (Removed "dsProfileVersion"...) which caused the vdcd to crash on first attempt to read a property

src/dsaddressable.cpp
@@ -221,7 +221,6 @@ void DsAddressable::checkPresence(PresenceCB aPresenceResultHandler)
 enum {
   dSID_key,
   model_key,
-  dsProfileVersion_key,
   hardwareVersion_key,
   hardwareGUID_key,
   numDevicesInHW_key,