plan44/vdcd

commit 9a1b3d4c51889c9202625640d71ace9e7f6e9512

vDC: fixed announce timing, was a bit irregular - now strictly 1 device per second

src/devicecontainer.cpp
@@ -338,10 +338,8 @@ void DeviceContainer::addDevice(DevicePtr aDevice)
   LOG(LOG_INFO, "- device description: %s",aDevice->description().c_str());
   // load the device's persistent params
   aDevice->load();
-  // unless collecting now, register new device right away
-  if (!collecting && sessionActive) {
-    announceDevices();
-  }
+  // register new device right away (unless collecting or already announcing)
+  announceDevices();
 }
 
 
@@ -411,10 +409,8 @@ void DeviceContainer::periodicTask(MLMicroSeconds aCycleStartTime)
   // cancel any pending executions
   MainLoop::currentMainLoop().cancelExecutionTicket(periodicTaskTicket);
   if (!collecting) {
-    if (sessionActive) {
-      // check for devices that need to be announced
-      announceDevices();
-    }
+    // check for devices that need to be announced
+    announceDevices();
     // do a save run as well
     for (DsDeviceMap::iterator pos = dSDevices.begin(); pos!=dSDevices.end(); ++pos) {
       pos->second->save();
@@ -856,35 +852,41 @@ void DeviceContainer::endContainerSession()
 #define ANNOUNCE_RETRY_TIMEOUT (300*Second)
 
 // how long vDC waits after receiving ok from one announce until it fires the next
-#define ANNOUNCE_PAUSE (2*Second)
+#define ANNOUNCE_PAUSE (1*Second)
 
 /// announce all not-yet announced devices to the vdSM
 void DeviceContainer::announceDevices()
 {
   if (!collecting && announcementTicket==0 && sessionActive) {
-    // check all devices for unnannounced ones and announce those
-    for (DsDeviceMap::iterator pos = dSDevices.begin(); pos!=dSDevices.end(); ++pos) {
-      DevicePtr dev = pos->second;
-      if (
+    announceNext();
+  }
+}
+
+
+void DeviceContainer::announceNext()
+{
+  // check all devices for unnannounced ones and announce those
+  for (DsDeviceMap::iterator pos = dSDevices.begin(); pos!=dSDevices.end(); ++pos) {
+    DevicePtr dev = pos->second;
+    if (
         dev->isPublicDS() && // only public ones
         dev->announced==Never &&
         (dev->announcing==Never || MainLoop::now()>dev->announcing+ANNOUNCE_RETRY_TIMEOUT)
-      ) {
-        // mark device as being in process of getting announced
-        dev->announcing = MainLoop::now();
-        // call announce method
-        if (!dev->sendRequest("announce", JsonObjectPtr(), boost::bind(&DeviceContainer::announceResultHandler, this, dev, _1, _2, _3, _4))) {
-          LOG(LOG_ERR, "Could not send announcement message for device %s\n", dev->shortDesc().c_str());
-          dev->announcing = Never; // not registering
-        }
-        else {
-          LOG(LOG_NOTICE, "Sent announcement for device %s\n", dev->shortDesc().c_str());
-        }
-        // schedule a retry
-        announcementTicket = MainLoop::currentMainLoop().executeOnce(boost::bind(&DeviceContainer::announceDevices, this), ANNOUNCE_TIMEOUT);
-        // done for now, continues after ANNOUNCE_TIMEOUT or when registration acknowledged
-        break;
+        ) {
+      // mark device as being in process of getting announced
+      dev->announcing = MainLoop::now();
+      // call announce method
+      if (!dev->sendRequest("announce", JsonObjectPtr(), boost::bind(&DeviceContainer::announceResultHandler, this, dev, _1, _2, _3, _4))) {
+        LOG(LOG_ERR, "Could not send announcement message for device %s\n", dev->shortDesc().c_str());
+        dev->announcing = Never; // not registering
       }
+      else {
+        LOG(LOG_NOTICE, "Sent announcement for device %s\n", dev->shortDesc().c_str());
+      }
+      // schedule a retry
+      announcementTicket = MainLoop::currentMainLoop().executeOnce(boost::bind(&DeviceContainer::announceNext, this), ANNOUNCE_TIMEOUT);
+      // done for now, continues after ANNOUNCE_TIMEOUT or when registration acknowledged
+      break;
     }
   }
 }
@@ -902,7 +904,7 @@ void DeviceContainer::announceResultHandler(DevicePtr aDevice, JsonRpcComm *aJso
   // cancel retry timer
   MainLoop::currentMainLoop().cancelExecutionTicket(announcementTicket);
   // try next announcement, after a pause
-  MainLoop::currentMainLoop().executeOnce(boost::bind(&DeviceContainer::announceDevices, this), ANNOUNCE_PAUSE);
+  announcementTicket = MainLoop::currentMainLoop().executeOnce(boost::bind(&DeviceContainer::announceNext, this), ANNOUNCE_PAUSE);
 }
 
 

src/devicecontainer.hpp
@@ -28,6 +28,9 @@ namespace p44 {
   typedef boost::intrusive_ptr<DeviceClassContainer> DeviceClassContainerPtr;
   typedef boost::intrusive_ptr<Device> DevicePtr;
 
+  /// generic callback for signalling something done
+  typedef boost::function<void ()> DoneCB;
+
   /// generic callback for signalling completion (with success/error reporting)
   typedef boost::function<void (ErrorPtr aError)> CompletedCB;
 
@@ -281,6 +284,7 @@ namespace p44 {
 
     // announcing devices
     void announceDevices();
+    void announceNext();
     void announceResultHandler(DevicePtr aDevice, JsonRpcComm *aJsonRpcComm, int32_t aResponseId, ErrorPtr &aError, JsonObjectPtr aResultOrErrorData);
 
   public:

commit 26b3395f0e1368677dcf3690a3f1eb3fe46b1802

New devices learned in were not announced any more - fixed. I broke this earlier with 9a1b3d4c51 (vDC: fixed announce timing, was a bit irregular - now strictly 1 device per second)
Problem was that announcementTicket was not reset in announceNext(), but it was tested for zero in announceDevices(), so already executed ticket did block *any* further announcements.

src/devicecontainer.cpp
@@ -901,14 +901,16 @@ void DeviceContainer::announceDevices()
 void DeviceContainer::announceNext()
 {
   if (collecting) return; // prevent announcements during collect.
+  // cancel re-announcing
+  MainLoop::currentMainLoop().cancelExecutionTicket(announcementTicket);
   // check all devices for unnannounced ones and announce those
   for (DsDeviceMap::iterator pos = dSDevices.begin(); pos!=dSDevices.end(); ++pos) {
     DevicePtr dev = pos->second;
     if (
-        dev->isPublicDS() && // only public ones
-        dev->announced==Never &&
-        (dev->announcing==Never || MainLoop::now()>dev->announcing+ANNOUNCE_RETRY_TIMEOUT)
-        ) {
+      dev->isPublicDS() && // only public ones
+      dev->announced==Never &&
+      (dev->announcing==Never || MainLoop::now()>dev->announcing+ANNOUNCE_RETRY_TIMEOUT)
+    ) {
       // mark device as being in process of getting announced
       dev->announcing = MainLoop::now();
       // call announce method