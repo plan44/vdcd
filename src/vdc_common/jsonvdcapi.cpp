7512d7d5f489945f80430bb995d16f6c8a6c49a5
Lukas Zeller <luz@plan44.ch>
2013-12-05 12:03:19 +0100

    jsonvdcapi.cpp/.h were missing in last commit

--- src/vdc_common/jsonvdcapi.cpp
+++ src/vdc_common/jsonvdcapi.cpp
@@ -6,4 +6,215 @@
 //  Copyright (c) 2013 plan44.ch. All rights reserved.
 //
 
-#include "jsonvdcapi.h"
+#include "jsonvdcapi.hpp"
+
+using namespace p44;
+
+
+
+#pragma mark - JsonApiValue
+
+JsonApiValue::JsonApiValue()
+{
+}
+
+
+ApiValuePtr JsonApiValue::newValue(ApiValueType aObjectType)
+{
+  ApiValuePtr newVal = ApiValuePtr(new JsonApiValue);
+  newVal->setType(aObjectType);
+  return newVal;
+}
+
+
+
+void JsonApiValue::clear()
+{
+  switch (getType()) {
+    case apivalue_object:
+      // just assign new object an forget old one
+      jsonObj = JsonObject::newObj();
+      break;
+    case apivalue_array:
+      jsonObj = JsonObject::newArray();
+      break;
+      // for unstuctured values, the json obj will be created on assign, so clear it now
+    default:
+      jsonObj.reset();
+      break;
+  }
+}
+
+
+bool JsonApiValue::setStringValue(const string &aString)
+{
+  if (getType()==apivalue_string) {
+    jsonObj = JsonObject::newString(aString, false);
+    return true;
+  }
+  else
+    return inherited::setStringValue(aString);
+};
+
+
+
+
+JsonApiValue::JsonApiValue(JsonObjectPtr aWithObject)
+{
+  jsonObj = aWithObject;
+  // derive type
+  if (!jsonObj) {
+    setType(apivalue_null);
+  }
+  else {
+    switch (jsonObj->type()) {
+      case json_type_boolean: objectType = apivalue_bool; break;
+      case json_type_double: objectType = apivalue_double; break;
+      case json_type_int: objectType = apivalue_int64; break;
+      case json_type_object: objectType = apivalue_object; break;
+      case json_type_array: objectType = apivalue_array; break;
+      case json_type_string: objectType = apivalue_string; break;
+      case json_type_null:
+      default:
+        setType(apivalue_null);
+        break;
+    }
+  }
+}
+
+
+ApiValuePtr JsonApiValue::newValueFromJson(JsonObjectPtr aJsonObject)
+{
+  return ApiValuePtr(new JsonApiValue(aJsonObject));
+}
+
+
+
+#pragma mark - VdcJsonApiServer
+
+
+VdcApiConnectionPtr VdcJsonApiServer::newConnection()
+{
+  // create the right kind of API connection
+  return VdcApiConnectionPtr(static_cast<VdcApiConnection *>(new VdcJsonApiConnection()));
+}
+
+
+
+#pragma mark - VdcJsonApiRequest
+
+
+VdcJsonApiRequest::VdcJsonApiRequest(VdcJsonApiConnectionPtr aConnection, const char *aJsonRpcId)
+{
+  jsonConnection = aConnection;
+  jsonRpcId = aJsonRpcId ? aJsonRpcId : ""; // empty if none passed
+}
+
+
+VdcApiConnectionPtr VdcJsonApiRequest::connection()
+{
+  return jsonConnection;
+}
+
+
+
+ErrorPtr VdcJsonApiRequest::sendResult(ApiValuePtr aResult)
+{
+  LOG(LOG_INFO,"vdSM <- vDC (JSON) result sent: requestid='%s', result=%s\n", requestId().c_str(), aResult ? aResult->description().c_str() : "<none>");
+  JsonApiValuePtr result = boost::dynamic_pointer_cast<JsonApiValue>(aResult);
+  return jsonConnection->jsonRpcComm->sendResult(requestId().c_str(), result ? result->jsonObject() : NULL);
+}
+
+
+ErrorPtr VdcJsonApiRequest::sendError(uint32_t aErrorCode, string aErrorMessage, ApiValuePtr aErrorData)
+{
+  LOG(LOG_INFO,"vdSM <- vDC (JSON) error sent: requestid='%s', error=%d (%s)\n", requestId().c_str(), aErrorCode, aErrorMessage.c_str());
+  JsonApiValuePtr errorData;
+  if (aErrorData)
+    errorData = boost::dynamic_pointer_cast<JsonApiValue>(aErrorData);
+  return jsonConnection->jsonRpcComm->sendError(requestId().c_str(), aErrorCode, aErrorMessage.size()>0 ? aErrorMessage.c_str() : NULL, errorData->jsonObject());
+}
+
+
+
+#pragma mark - VdcJsonApiConnection
+
+
+VdcJsonApiConnection::VdcJsonApiConnection()
+{
+  jsonRpcComm = JsonRpcCommPtr(new JsonRpcComm(SyncIOMainLoop::currentMainLoop()));
+  // install JSON request handler locally
+  jsonRpcComm->setRequestHandler(boost::bind(&VdcJsonApiConnection::jsonRequestHandler, this, _1, _2, _3, _4));
+}
+
+
+
+void VdcJsonApiConnection::jsonRequestHandler(JsonRpcComm *aJsonRpcComm, const char *aMethod, const char *aJsonRpcId, JsonObjectPtr aParams)
+{
+  ErrorPtr respErr;
+  if (apiRequestHandler) {
+    // create params API value
+    ApiValuePtr params = JsonApiValue::newValueFromJson(aParams);
+    VdcApiRequestPtr request;
+    // create request object in case this request expects an answer
+    if (aJsonRpcId) {
+      // Method
+      request = VdcJsonApiRequestPtr(new VdcJsonApiRequest(VdcJsonApiConnectionPtr(this), aJsonRpcId));
+      LOG(LOG_INFO,"vdSM -> vDC (JSON) method call '%s' received: requestid='%s', params=%s\n", aMethod, request->requestId().c_str(), params ? params->description().c_str() : "<none>");
+    }
+    else {
+      // Notification
+      LOG(LOG_INFO,"vdSM -> vDC (JSON) notification '%s' received: params=%s\n", aMethod, params ? params->description().c_str() : "<none>");
+    }
+    // call handler
+    apiRequestHandler(VdcJsonApiConnectionPtr(this), request, aMethod, params);
+  }
+}
+
+
+void VdcJsonApiConnection::closeAfterSend()
+{
+  jsonRpcComm->closeAfterSend();
+}
+
+
+ApiValuePtr VdcJsonApiConnection::newApiValue()
+{
+  return ApiValuePtr(new JsonApiValue);
+}
+
+
+ErrorPtr VdcJsonApiConnection::sendRequest(const string &aMethod, ApiValuePtr aParams, VdcApiResponseCB aResponseHandler)
+{
+  JsonApiValuePtr params = boost::dynamic_pointer_cast<JsonApiValue>(aParams);
+  ErrorPtr err;
+  if (aResponseHandler) {
+    // method call expecting response
+    err = jsonRpcComm->sendRequest(aMethod.c_str(), params->jsonObject(), boost::bind(&VdcJsonApiConnection::jsonResponseHandler, this, aResponseHandler, _2, _3, _4));
+    LOG(LOG_INFO,"vdSM <- vDC (JSON) method call sent: requestid='%d', method='%s', params=%s\n", jsonRpcComm->lastRequestId(), aMethod.c_str(), aParams ? aParams->description().c_str() : "<none>");
+  }
+  else {
+    // notification
+    err = jsonRpcComm->sendRequest(aMethod.c_str(), params->jsonObject(), NULL);
+    LOG(LOG_INFO,"vdSM <- vDC (JSON) notification sent: method='%s', params=%s\n", aMethod.c_str(), aParams ? aParams->description().c_str() : "<none>");
+  }
+  return err;
+}
+
+
+void VdcJsonApiConnection::jsonResponseHandler(VdcApiResponseCB aResponseHandler, int32_t aResponseId, ErrorPtr &aError, JsonObjectPtr aResultOrErrorData)
+{
+  if (aResponseHandler) {
+    // create request object just to hold the response ID
+    string respId = string_format("%d", aResponseId);
+    ApiValuePtr resultOrErrorData = JsonApiValue::newValueFromJson(aResultOrErrorData);
+    VdcApiRequestPtr request = VdcJsonApiRequestPtr(new VdcJsonApiRequest(VdcJsonApiConnectionPtr(this), respId.c_str()));
+    if (Error::isOK(aError)) {
+      LOG(LOG_INFO,"vdSM -> vDC (JSON) result received: id='%d', result=%s\n", request->requestId().c_str(), resultOrErrorData ? resultOrErrorData->description().c_str() : "<none>");
+    }
+    else {
+      LOG(LOG_INFO,"vdSM -> vDC (JSON) error received: id='%d', error=%s, errordata=%s\n", request->requestId().c_str(), aError->description().c_str(), resultOrErrorData ? resultOrErrorData->description().c_str() : "<none>");
+    }
+    aResponseHandler(VdcApiConnectionPtr(this), request, aError, resultOrErrorData);
+  }
+}
--- src/vdc_common/jsonvdcapi.hpp
+++ src/vdc_common/jsonvdcapi.hpp
@@ -1,5 +1,5 @@
 //
-//  jsonvdcapi.h
+//  jsonvdcapi.hpp
 //  vdcd
 //
 //  Created by Lukas Zeller on 05.12.13.
@@ -9,6 +9,174 @@
 #ifndef __vdcd__jsonvdcapi__
 #define __vdcd__jsonvdcapi__
 
-#include <iostream>
+#include "p44_common.hpp"
+
+#include "vdcapi.hpp"
+
+#include "jsonrpccomm.hpp"
+
+using namespace std;
+
+namespace p44 {
+
+  class VdcJsonApiConnection;
+  class VdcJsonApiServer;
+  class VdcJsonApiRequest;
+
+  typedef boost::intrusive_ptr<VdcJsonApiConnection> VdcJsonApiConnectionPtr;
+  typedef boost::intrusive_ptr<VdcJsonApiServer> VdcJsonApiServerPtr;
+  typedef boost::intrusive_ptr<VdcJsonApiRequest> VdcJsonApiRequestPtr;
+
+
+
+  class JsonApiValue;
+
+  typedef boost::intrusive_ptr<JsonApiValue> JsonApiValuePtr;
+
+  class JsonApiValue : public ApiValue
+  {
+    typedef ApiValue inherited;
+
+    // using an embedded Json Object
+    JsonObjectPtr jsonObj;
+
+    JsonApiValue(JsonObjectPtr aWithObject);
+
+  public:
+
+    JsonApiValue();
+
+    virtual ApiValuePtr newValue(ApiValueType aObjectType);
+
+    static ApiValuePtr newValueFromJson(JsonObjectPtr aJsonObject);
+
+    virtual void clear();
+
+    virtual void add(const string &aKey, ApiValuePtr aObj) { JsonApiValuePtr o = boost::dynamic_pointer_cast<JsonApiValue>(aObj); if (jsonObj && o) jsonObj->add(aKey.c_str(), o->jsonObject()); };
+    virtual ApiValuePtr get(const string &aKey)  { JsonObjectPtr o; if (jsonObj && jsonObj->get(aKey.c_str(), o)) return newValueFromJson(o); else return ApiValuePtr(); };
+    virtual void del(const string &aKey) { if (jsonObj) jsonObj->del(aKey.c_str()); };
+    virtual int arrayLength() { return jsonObj ? jsonObj->arrayLength() : 0; };
+    virtual void arrayAppend(ApiValuePtr aObj) { JsonApiValuePtr o = boost::dynamic_pointer_cast<JsonApiValue>(aObj); if (jsonObj && o) jsonObj->arrayAppend(o->jsonObject()); };
+    virtual ApiValuePtr arrayGet(int aAtIndex) { if (jsonObj) { JsonObjectPtr o = jsonObj->arrayGet(aAtIndex); return newValueFromJson(o); } else return ApiValuePtr(); };
+    virtual void arrayPut(int aAtIndex, ApiValuePtr aObj) { JsonApiValuePtr o = boost::dynamic_pointer_cast<JsonApiValue>(aObj); if (jsonObj && o) jsonObj->arrayPut(aAtIndex, o->jsonObject()); };
+    virtual bool resetKeyIteration() { if (jsonObj) return jsonObj->resetKeyIteration(); else return false; };
+    virtual bool nextKeyValue(string &aKey, ApiValuePtr &aValue) { if (jsonObj) { JsonObjectPtr o; bool gotone = jsonObj->nextKeyValue(aKey, o); aValue = newValueFromJson(o); return gotone; } else return false; };
+
+    virtual uint64_t uint64Value() { return jsonObj ? (uint64_t)jsonObj->int64Value() : 0; };
+    virtual int64_t int64Value() { return jsonObj ? jsonObj->int64Value() : 0; };
+    virtual double doubleValue() { return jsonObj ? jsonObj->doubleValue() : 0; };
+    virtual bool boolValue() { return jsonObj ? jsonObj->boolValue() : false; };
+    virtual string stringValue() { if (getType()==apivalue_string) { return jsonObj ? jsonObj->stringValue() : ""; } else return inherited::stringValue(); };
+
+    virtual void setUint64Value(uint64_t aUint64) { jsonObj = JsonObject::newInt64(aUint64); }
+    virtual void setInt64Value(int64_t aInt64) { jsonObj = JsonObject::newInt64(aInt64); };
+    virtual void setDoubleValue(double aDouble) { jsonObj = JsonObject::newDouble(aDouble); };
+    virtual void setBoolValue(bool aBool) { jsonObj = JsonObject::newBool(aBool); };
+    virtual bool setStringValue(const string &aString);
+    virtual void setNull() { jsonObj.reset(); }
+
+    JsonObjectPtr jsonObject() { return jsonObj; };
+
+  protected:
+    
+    
+  };
+
+
+  /// a JSON API server
+  class VdcJsonApiServer : public VdcApiServer
+  {
+    typedef VdcApiServer inherited;
+
+  protected:
+
+    /// create API connection of correct type for this API server
+    /// @return API connection
+    virtual VdcApiConnectionPtr newConnection();
+
+  };
+
+
+
+  class VdcJsonApiRequest : public VdcApiRequest
+  {
+    typedef VdcApiRequest inherited;
+
+    string jsonRpcId;
+    VdcJsonApiConnectionPtr jsonConnection;
+
+  public:
+
+    /// constructor
+    VdcJsonApiRequest(VdcJsonApiConnectionPtr aConnection, const char *aJsonRpcId);
+
+    /// return the request ID as a string
+    /// @return request ID as string
+    virtual string requestId() { return jsonRpcId; }
+
+    /// get the API connection this request originates from
+    /// @return API connection
+    virtual VdcApiConnectionPtr connection();
+
+    /// send a vDC API result (answer for successful method call)
+    /// @param aResult the result as a ApiValue. Can be NULL for procedure calls without return value
+    /// @result empty or Error object in case of error sending result response
+    virtual ErrorPtr sendResult(ApiValuePtr aResult);
+
+    /// send a vDC API error (answer for unsuccesful method call)
+    /// @param aErrorCode the error code
+    /// @param aErrorMessage the error message or NULL to generate a standard text
+    /// @param aErrorData the optional "data" member for the vDC API error object
+    /// @result empty or Error object in case of error sending error response
+    virtual ErrorPtr sendError(uint32_t aErrorCode, string aErrorMessage = "", ApiValuePtr aErrorData = ApiValuePtr());
+
+  };
+
+
+
+  class VdcJsonApiConnection : public VdcApiConnection
+  {
+    typedef VdcApiConnection inherited;
+
+    friend class VdcJsonApiRequest;
+
+    JsonRpcCommPtr jsonRpcComm;
+
+  public:
+
+    VdcJsonApiConnection();
+
+    /// The underlying socket connection
+    /// @return socket connection
+    virtual SocketCommPtr socketConnection() { return jsonRpcComm; };
+
+    /// request closing connection after last message has been sent
+    virtual void closeAfterSend();
+
+    /// get a new API value suitable for this connection
+    /// @return new API value of suitable internal implementation to be used on this API connection
+    virtual ApiValuePtr newApiValue();
+
+    /// send a API request
+    /// @param aMethod the vDC API method or notification name to be sent
+    /// @param aParams the parameters for the method or notification request. Can be NULL.
+    /// @param aResponseHandler if the request is a method call, this handler will be called when the method result arrives
+    ///   Note that the aResponseHandler might not be called at all in case of lost messages etc. So do not rely on
+    ///   this callback for chaining a execution thread.
+    /// @return empty or Error object in case of error
+    virtual ErrorPtr sendRequest(const string &aMethod, ApiValuePtr aParams, VdcApiResponseCB aResponseHandler = VdcApiResponseCB());
+
+  private:
+
+    void jsonRequestHandler(JsonRpcComm *aJsonRpcComm, const char *aMethod, const char *aJsonRpcId, JsonObjectPtr aParams);
+    void jsonResponseHandler(VdcApiResponseCB aResponseHandler, int32_t aResponseId, ErrorPtr &aError, JsonObjectPtr aResultOrErrorData);
+
+  };
+
+
+}
+
+
+
 
 #endif /* defined(__vdcd__jsonvdcapi__) */
