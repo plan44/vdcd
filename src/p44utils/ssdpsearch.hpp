b3fe779cf6add7995f3791ab4bfd9b275c40ae28
Lukas Zeller <luz@plan44.ch>
2013-12-09 13:37:21 +0100

    GPLv3 headers in .hpp files as well

--- src/p44utils/ssdpsearch.hpp
+++ src/p44utils/ssdpsearch.hpp
@@ -1,9 +1,22 @@
 //
-//  ssdpsearch.hpp
-//  p44utils
+//  Copyright (c) 2013 plan44.ch / Lukas Zeller, Zurich, Switzerland
 //
-//  Created by Lukas Zeller on 02.09.13.
-//  Copyright (c) 2013 plan44.ch. All rights reserved.
+//  Author: Lukas Zeller <luz@plan44.ch>
+//
+//  This file is part of p44utils.
+//
+//  p44utils is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  p44utils is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with p44utils. If not, see <http://www.gnu.org/licenses/>.
 //
 
 #ifndef __vdcd__ssdpsearch__
