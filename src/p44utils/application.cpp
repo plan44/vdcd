87c21591fd648557cac6fb76ea0340cc4d71df37
Lukas Zeller <luz@plan44.ch>
2013-12-09 13:19:54 +0100

    Prepared file headers and root files for GPLv3 release (BUT THAT HAS NOT YET HAPPENED!)

--- src/p44utils/application.cpp
+++ src/p44utils/application.cpp
@@ -1,11 +1,23 @@
 //
-//  application.cpp
-//  p44utils
+//  Copyright (c) 2013 plan44.ch / Lukas Zeller, Zurich, Switzerland
 //
-//  Created by Lukas Zeller on 02.05.13.
-//  Copyright (c) 2013 plan44.ch. All rights reserved.
+//  Author: Lukas Zeller <luz@plan44.ch>
 //
-
+//  This file is part of p44utils.
+//
+//  p44utils is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  p44utils is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with p44utils. If not, see <http://www.gnu.org/licenses/>.
+//
 #include "application.hpp"
 
 #include "mainloop.hpp"
@@ -76,10 +88,10 @@ void Application::terminateApp(int aExitCode)
 void Application::daemonize()
 {
   pid_t pid, sid;
-	
+
   /* already a daemon */
   if ( getppid() == 1 ) return;
-	
+
   /* Fork off the parent process */
   pid = fork();
   if (pid < 0) {
@@ -89,24 +101,24 @@ void Application::daemonize()
   if (pid > 0) {
     exit(EXIT_SUCCESS);
   }
-	
+
   /* At this point we are executing as the child process */
-	
+
   /* Change the file mode mask */
   umask(0);
-	
+
   /* Create a new SID for the child process */
   sid = setsid();
   if (sid < 0) {
     exit(EXIT_FAILURE);
   }
-	
+
   /* Change the current working directory.  This prevents the current
 	 directory from being locked; hence not being able to remove it. */
   if ((chdir("/")) < 0) {
     exit(EXIT_FAILURE);
   }
-	
+
   /* Redirect standard files to /dev/null */
   freopen( "/dev/null", "r", stdin);
   freopen( "/dev/null", "w", stdout);
