plan44/vdcd

commit dbcdca7c1d1ec9932395109a4c7ccae6533ad4c9

p44 config API: make sure pending connections are closed when learn/identify is terminated

src/vdc_common/p44_vdcd_host.cpp
@@ -408,6 +408,7 @@ ErrorPtr P44VdcHost::processP44Request(JsonCommPtr aJsonComm, JsonObjectPtr aReq
       }
       else {
         // start learning
+        learnIdentifyRequest = aJsonComm; // remember so we can cancel it when we receive a separate cancel request
         startLearning(boost::bind(&P44VdcHost::learnHandler, this, aJsonComm, _1, _2), disableProximity);
         learnIdentifyTicket = MainLoop::currentMainLoop().executeOnce(boost::bind(&P44VdcHost::learnHandler, this, aJsonComm, false, ErrorPtr(new P44VdcError(408, "learn timeout"))), seconds*Second);
       }
@@ -420,9 +421,11 @@ ErrorPtr P44VdcHost::processP44Request(JsonCommPtr aJsonComm, JsonObjectPtr aReq
       if (seconds==0) {
         // end reporting user activity
         setUserActionMonitor(NULL);
+        identifyHandler(aJsonComm, DevicePtr());
       }
       else {
         // wait for next user activity
+        learnIdentifyRequest = aJsonComm; // remember so we can cancel it when we receive a separate cancel request
         setUserActionMonitor(boost::bind(&P44VdcHost::identifyHandler, this, aJsonComm, _1));
         learnIdentifyTicket = MainLoop::currentMainLoop().executeOnce(boost::bind(&P44VdcHost::identifyHandler, this, aJsonComm, DevicePtr()), seconds*Second);
       }
@@ -451,6 +454,10 @@ ErrorPtr P44VdcHost::processP44Request(JsonCommPtr aJsonComm, JsonObjectPtr aReq
 void P44VdcHost::learnHandler(JsonCommPtr aJsonComm, bool aLearnIn, ErrorPtr aError)
 {
   MainLoop::currentMainLoop().cancelExecutionTicket(learnIdentifyTicket);
+  if (learnIdentifyRequest) {
+    learnIdentifyRequest->closeConnection();
+    learnIdentifyRequest.reset();
+  }
   stopLearning();
   sendCfgApiResponse(aJsonComm, JsonObject::newBool(aLearnIn), aError);
 }
@@ -466,6 +473,10 @@ void P44VdcHost::identifyHandler(JsonCommPtr aJsonComm, DevicePtr aDevice)
   }
   else {
     sendCfgApiResponse(aJsonComm, JsonObjectPtr(), ErrorPtr(new P44VdcError(408, "identify timeout")));
+    if (learnIdentifyRequest) {
+      learnIdentifyRequest->closeConnection();
+      learnIdentifyRequest.reset();
+    }
     setUserActionMonitor(NULL);
   }
 }

src/vdc_common/p44_vdcd_host.hpp
@@ -88,6 +88,7 @@ namespace p44 {
     friend class P44JsonApiRequest;
 
     long learnIdentifyTicket;
+    JsonCommPtr learnIdentifyRequest;
 
   public:
 

commit 7dde6d0a91f1445afbfb9e3356f41cb624895f3c

Fixed Web-UI initiated learn and identify - which was broken since dbcdca7c1d (p44 config API: make sure pending connections are closed when learn/identify is terminated))

src/vdc_common/p44_vdcd_host.cpp
@@ -403,9 +403,16 @@ ErrorPtr P44VdcHost::processP44Request(JsonCommPtr aJsonComm, JsonObjectPtr aReq
       int seconds = 30; // default to 30
       if (o) seconds = o->int32Value();
       if (seconds==0) {
-        // end learning
+        // end learning prematurely
         stopLearning();
-        learnHandler(aJsonComm, false, ErrorPtr());
+        MainLoop::currentMainLoop().cancelExecutionTicket(learnIdentifyTicket);
+        // - close still running learn request
+        if (learnIdentifyRequest) {
+          learnIdentifyRequest->closeConnection();
+          learnIdentifyRequest.reset();
+        }
+        // - confirm abort with no result
+        sendCfgApiResponse(aJsonComm, JsonObjectPtr(), ErrorPtr());
       }
       else {
         // start learning
@@ -422,7 +429,14 @@ ErrorPtr P44VdcHost::processP44Request(JsonCommPtr aJsonComm, JsonObjectPtr aReq
       if (seconds==0) {
         // end reporting user activity
         setUserActionMonitor(NULL);
-        identifyHandler(aJsonComm, DevicePtr());
+        MainLoop::currentMainLoop().cancelExecutionTicket(learnIdentifyTicket);
+        // - close still running identify request
+        if (learnIdentifyRequest) {
+          learnIdentifyRequest->closeConnection();
+          learnIdentifyRequest.reset();
+        }
+        // - confirm abort with no result
+        sendCfgApiResponse(aJsonComm, JsonObjectPtr(), ErrorPtr());
       }
       else {
         // wait for next user activity
@@ -455,12 +469,9 @@ ErrorPtr P44VdcHost::processP44Request(JsonCommPtr aJsonComm, JsonObjectPtr aReq
 void P44VdcHost::learnHandler(JsonCommPtr aJsonComm, bool aLearnIn, ErrorPtr aError)
 {
   MainLoop::currentMainLoop().cancelExecutionTicket(learnIdentifyTicket);
-  if (learnIdentifyRequest) {
-    learnIdentifyRequest->closeConnection();
-    learnIdentifyRequest.reset();
-  }
   stopLearning();
   sendCfgApiResponse(aJsonComm, JsonObject::newBool(aLearnIn), aError);
+  learnIdentifyRequest.reset();
 }
 
 
@@ -474,12 +485,9 @@ void P44VdcHost::identifyHandler(JsonCommPtr aJsonComm, DevicePtr aDevice)
   }
   else {
     sendCfgApiResponse(aJsonComm, JsonObjectPtr(), ErrorPtr(new P44VdcError(408, "identify timeout")));
-    if (learnIdentifyRequest) {
-      learnIdentifyRequest->closeConnection();
-      learnIdentifyRequest.reset();
-    }
     setUserActionMonitor(NULL);
   }
+  learnIdentifyRequest.reset();
 }
 
 