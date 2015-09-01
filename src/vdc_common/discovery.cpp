859fa40a49b3ce8399a28549365bec5327b98e4b
Lukas Zeller <luz@plan44.ch>
2015-09-01 08:15:18 +0200

    Discovery: better log message ("appear" is the wrong term, because avahi re-scans from time to time even and detects already appeared vdsms again)

--- src/vdc_common/discovery.cpp
+++ src/vdc_common/discovery.cpp
@@ -565,7 +565,7 @@ void DiscoveryManager::avahi_browse_callback(AvahiSServiceBrowser *b, AvahiIfInd
         // Note: local vdsm record (my own) is ignored
         if (strcmp(type, VDSM_SERVICE_TYPE)==0 && (flags & AVAHI_LOOKUP_RESULT_LOCAL)==0) {
           // new vdsm found. Only for vdsms, we need to resolve to get the TXT records in order to see if we've found a master vdsm
-          LOG(LOG_NOTICE, "discovery: vdsm '%s' has appeared\n", name);
+          LOG(LOG_NOTICE, "discovery: vdsm '%s' detected\n", name);
           // Note: the returned resolver object can be ignored, it is freed in the callback
           //   if the server terminates before the callback has been executes, the server deletes the resolver.
           if (!(avahi_s_service_resolver_new(server, interface, protocol, name, type, domain, AVAHI_PROTO_UNSPEC, (AvahiLookupFlags)0, resolve_callback, this))) {
