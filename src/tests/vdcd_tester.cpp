c077d23e01729f1be85888e41bdc5ebdf5bf9435
Lukas Zeller <luz@plan44.ch>
2016-06-21 18:40:35 +0200

    testing: first experiments with Catch testing framework (included now in p44utils)
    see https://github.com/philsquared/Catch
    
    My reasons to use Catch
    - dependency free
    - header only

---
+++ src/tests/vdcd_tester.cpp
@@ -0,0 +1,23 @@
+//
+//  Copyright (c) 2016 plan44.ch / Lukas Zeller, Zurich, Switzerland
+//
+//  Author: Lukas Zeller <luz@plan44.ch>
+//
+//  This file is part of vdcd.
+//
+//  p44utils is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  p44utils is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with p44utils. If not, see <http://www.gnu.org/licenses/>.
+//
+
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
