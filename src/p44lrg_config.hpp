21871ad5a1dcd46ba4ff7af26e46af8a4bef1d55
Lukas Zeller <luz@plan44.ch>
2019-09-07 20:09:59 +0000

    Add autoconf option for libpng (ENABLE_PNG) - if enabled, p44lrgraphics has image support

---
+++ src/p44lrg_config.hpp
@@ -0,0 +1,34 @@
+//
+//  Copyright (c) 2016-2019 plan44.ch / Lukas Zeller, Zurich, Switzerland
+//
+//  Author: Lukas Zeller <luz@plan44.ch>
+//
+//  This file is part of p44lrgraphics.
+//
+//  p44lrgraphics is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  p44lrgraphics is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with p44lrgraphics. If not, see <http://www.gnu.org/licenses/>.
+//
+
+
+#ifndef __p44lrgraphics__config__
+#define __p44lrgraphics__config__
+
+#define ENABLE_VIEWCONFIG 1
+#if ENABLE_PNG
+  #define ENABLE_IMAGE_SUPPORT 1
+#else
+  #define ENABLE_IMAGE_SUPPORT 0
+#endif
+
+
+#endif // __p44lrgraphics__config__
