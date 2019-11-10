306be17238600b149232cd7fb9039b3d1b43514e
Lukas Zeller <luz@plan44.ch>
2019-11-10 20:44:54 +0100

    p44utils: now need project-level p44utils_config.hpp

---
+++ src/p44utils_config.hpp
@@ -0,0 +1,16 @@
+//
+//  Copyright (c) 2019 plan44.ch / Lukas Zeller, Zurich, Switzerland
+//
+
+#ifndef __p44utils__config__
+#define __p44utils__config__
+
+#ifndef ENABLE_EXPRESSIONS
+  #define ENABLE_EXPRESSIONS 1 // Expression/Script engine support in some of the p44utils components
+#endif
+#ifndef ENABLE_P44LRGRAPHICS
+  #define ENABLE_P44LRGRAPHICS 1 // p44lrgraphics support in some of the p44utils components
+#endif
+
+
+#endif // __p44utils__config__
